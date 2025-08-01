c3a1ccd58c550fd2c3a7d1bb286c66e79852ca43
guliyev-islam <islam.guliyev@proton.me>
2025-08-01 18:14:10 +0400

    0x01/even/EvenNumbers.java

---
+++ 0x01/even/EvenNumbers.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 98; i += 2) {
+            System.out.print(i + ", ");
+        }
+        System.out.print(i + "\n");
+    }
+}
