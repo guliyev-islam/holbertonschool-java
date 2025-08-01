29b4b35c016e850e319ab33c75821b3600d216c9
guliyev-islam <islam.guliyev@proton.me>
2025-08-01 19:25:12 +0400

    Refactor

--- 0x01/uppercase/Character.java
+++ 0x01/uppercase/Character.java
@@ -1,10 +1,5 @@
 public class Character {
 	public static boolean IsUpper(char c) {
-	    if (c > 'A' && c < 'Z') {
-					return true;
-		}
-		else {
-		    return false;
-		}
+	    return c > 'A' && c < 'Z';
 	}
 }
