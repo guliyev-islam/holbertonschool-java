a5fdc5dd288004419f20f1a5ee2db2f95e1b1d81
guliyev-islam <islam.guliyev@proton.me>
2025-08-01 21:03:57 +0400

    The result of the division should be printed inside the finally block with the format shown in the example.

--- 0x02/division/Number.java
+++ 0x02/division/Number.java
@@ -1,19 +1,4 @@
-/*
-public class TheNumber {
-    
-    public static void todivide(int a, int b) {
-        int result = 0;
-        try {
-            result = a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("It is not possible to divide by zero.");
-        } finally {
-            System.out.printf("%d / %d = %d\n", a, b, result);
-        }
-    }
-}
-*/
-
+// class TheNumber
 public class Number {
     
     public static void todivide(int a, int b) {
