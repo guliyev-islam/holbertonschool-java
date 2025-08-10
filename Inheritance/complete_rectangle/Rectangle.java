6ea667a70146e4b9b5f9588513f107d37fc82819
guliyev-islam <islam.guliyev@proton.me>
2025-08-10 23:58:41 +0400

    Add Inheritance/complete_rectangle/*

---
+++ Inheritance/complete_rectangle/Rectangle.java
@@ -0,0 +1,39 @@
+public class Rectangle extends GeometricShape {
+
+    private double width;
+    private double height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) throws IllegalArgumentException {
+        if (width < 0) {
+            throw new IllegalArgumentException("Width must be greater than or equal to 0");
+        }
+
+        this.width = width;
+    }
+
+    public void setHeight(double height) throws IllegalArgumentException {
+        if (width < 0) {
+            throw new IllegalArgumentException("Height must be greater than or equal to 0");
+        }
+        
+        this.height = height;
+    }
+    
+    @Override
+    public double area() {
+        return getWidth() * getHeight();
+    }
+    
+    public String toString() { 
+        return String.format("[Rectangle] %.2f / %.2f", getWidth(), getHeight());
+    }
+    
+}
