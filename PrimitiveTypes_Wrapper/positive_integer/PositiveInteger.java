66832568bc8fb8205f82967e6c706a8dd0d45442
guliyev-islam <islam.guliyev@proton.me>
2025-08-28 12:37:43 +0400

    Add PrimitiveTypes_Wrapper/positive_integer/*

---
+++ PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
@@ -0,0 +1,31 @@
+public class PositiveInteger {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue (int value) throws IllegalArgumentException {    
+        if (value < 0) throw new IllegalArgumentException("Value is not a positive integer");
+        this.value = value;
+    }
+
+    public PositiveInteger(int value) throws IllegalArgumentException {
+        setValue(Integer.valueOf(value));
+    }
+
+    public PositiveInteger(String value) throws IllegalArgumentException{
+        setValue(Integer.valueOf(value));
+    }
+
+    public boolean isPrime() {
+            if (getValue() <= 1) return false;
+
+            for (int i = 2; i <= Math.sqrt(value); i++) {
+                if (getValue() % i == 0) return false;
+            }
+
+            return true;
+    }
+}
