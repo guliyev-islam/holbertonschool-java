5e25568044bc279cf40b49792dcd5f3f3ceef9ed
guliyev-islam <islam.guliyev@proton.me>
2025-09-08 11:37:57 +0400

    Add Set_Map/duplicates/*

---
+++ Set_Map/duplicates/DuplicatedNumbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class DuplicatedNumbers {
+
+    static public TreeSet<Integer> isFind(int[] integers) {
+        TreeMap<Integer, Integer> frequency = new TreeMap<Integer,Integer>();
+
+        for (Integer integer: integers) {
+            if (!frequency.containsKey(integer)) {
+                frequency.put(integer, 0);
+            }
+
+            frequency.put(integer, frequency.get(integer) + 1);
+        }
+
+        TreeSet<Integer> duplicates = new TreeSet<Integer>();
+
+        for (Integer integer: frequency.keySet()) {
+            if (frequency.get(integer) > 1) {
+                duplicates.add(integer);
+            }
+        }
+
+        return duplicates;
+    }
+}
