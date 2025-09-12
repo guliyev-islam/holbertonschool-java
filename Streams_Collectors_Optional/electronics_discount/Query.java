24b0b22697d60658d797be642a302e4d93b7ca52
guliyev-islam <islam.guliyev@proton.me>
2025-09-13 01:31:22 +0400

    Add Streams_Collectors_Optional/electronics_discount/*

---
+++ Streams_Collectors_Optional/electronics_discount/Query.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Query {
+
+    public static List<Product> getBooksFromOrder(Order order) {
+        return order.getProducts().stream().filter(product -> product.getCategory().equals(ProductCategory.BOOK))
+                .collect(Collectors.toList());
+    }
+
+    public static Product getProductWithHighestPrice(List<Product> products) {
+
+        List<Product> mutable = new ArrayList<>(products);
+        mutable.sort(Comparator.comparing(Product::getPrice).reversed());
+        return mutable.get(0);
+    }
+
+    public static List<Product> getProductsByMinimumPrice(List<Product> products, double minimumPrice) {
+        return products.stream().filter(product -> product.getPrice() >= minimumPrice).collect(Collectors.toList());
+    }
+
+    public static List<Order> getOrdersWithElectronics(List<Order> orders) {
+        return orders.stream().filter(order -> order.getProducts().stream()
+                .anyMatch(product -> product.getCategory() == ProductCategory.ELECTRONIC)).toList();
+    }
+
+
+    public static List<Product> apply15PercentDiscountToElectronics(List<Product> products) {
+        return products.stream().map(product -> {
+            if (product.getCategory().equals(ProductCategory.ELECTRONIC)) {
+                product.setPrice(product.getPrice() - product.getPrice() * 15 / 100);
+            }
+
+            return product;
+        }).toList();
+    }
+}
