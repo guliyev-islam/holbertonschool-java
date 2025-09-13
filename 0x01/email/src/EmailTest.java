dea105a2be1a8081f72d096c260158dc452811d6
guliyev-islam <islam.guliyev@proton.me>
2025-09-13 14:51:54 +0400

    Method test_email_without_at_symbol - use assertTrue

--- 0x01/email/src/EmailTest.java
+++ 0x01/email/src/EmailTest.java
@@ -7,10 +7,10 @@ public class EmailTest {
 	}
 
 	public static void test_email_without_at_symbol() {
-		assertFalse(Person.isEmailValid("email_testdomain.com"));
+		assertTrue(Person.isEmailValid("email_testdomain.com"));
 	}
 
 	public static void test_email_over_50_characters() {
-		assertFalse(Person.isEmailValid("email_test_very_long_should_not_be_valid@domain.com"));
+		assertEqual(Person.isEmailValid("email_test_very_long_should_not_be_valid@domain.com"));
 	}
 }
