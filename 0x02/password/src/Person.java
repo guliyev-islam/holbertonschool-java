b5a44ef6c2c7341ace615e44ce9147956f65aed0
guliyev-islam <islam.guliyev@proton.me>
2025-09-13 20:07:28 +0400

    Add 0x02/password/src/*

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,22 @@
+package com.iq;
+
+import java.io.StringReader;
+
+public class Person {
+    public boolean checkUser(String name) {
+        if (!name.matches("[a-zA-Z0-9]+")) {
+            return false;
+        }
+
+
+        return name.length() >= 8;
+    }
+
+    public boolean checkPassword(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+        return password != null && password.matches(regex);
+    }
+}
---
+++ 0x02/password/src/PersonTest.java
@@ -0,0 +1,53 @@
+package com.iq;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import java.time.*;
+
+public class PersonTest {
+
+    private static Person person;
+
+    @BeforeAll
+    public static void setup() {
+        person = new Person();
+    }
+
+    @Test
+    public void check_user_valid() {
+        assertTrue(person.checkUser("PaulMcCartney2"));
+        assertTrue(person.checkUser("NeilArms2"));
+    }
+
+    @Test
+    public void check_user_not_valid() {
+        assertFalse(person.checkUser("Paul#McCartney"));
+        assertFalse(person.checkUser("Neil@Arms"));
+    }
+
+    @Test
+    public void does_not_have_letters() {
+        assertFalse(person.checkPassword("123456789"));
+        assertFalse(person.checkPassword("#$%1234"));
+    }
+
+    @Test
+    public void does_not_have_numbers() {
+        assertFalse(person.checkPassword("Abcabcdefgh@"));
+        assertFalse(person.checkPassword("#hbtn@%tc"));
+    }
+
+    @Test
+    public void does_not_have_eight_chars() {
+        assertFalse(person.checkPassword("Abc@123"));
+        assertFalse(person.checkPassword("12$@hbt"));
+    }
+
+    @Test
+    public void check_password_valid() {
+        assertTrue(person.checkPassword("abC123456$"));
+        assertTrue(person.checkPassword("Hbtn@1234"));
+        assertTrue(person.checkPassword("Betty@1#2"));
+        assertTrue(person.checkPassword("Hbtn@123"));
+    }
+}
