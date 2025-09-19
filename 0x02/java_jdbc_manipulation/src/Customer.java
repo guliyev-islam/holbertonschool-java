d633aeef7ef7668c1e386087937181350a0f6eac
guliyev-islam <islam.guliyev@proton.me>
2025-09-19 21:21:19 +0400

    Add 0x02/java_jdbc_manipulation/*

---
+++ 0x02/java_jdbc_manipulation/src/Customer.java
@@ -0,0 +1,47 @@
+public class Customer {
+	private Integer id;
+	private String name;
+	private Integer age;
+	private String cpf;
+	private String rg;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+}
---
+++ 0x02/java_jdbc_manipulation/src/CustomerDAOImpl.java
@@ -0,0 +1,3 @@
+public class CustomerDAOImpl implements CustomerDAO {
+    
+}
