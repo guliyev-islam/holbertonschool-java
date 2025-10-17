b26e90e43fc520fb38dcf4ceeb9b7f50b49c3143
guliyev-islam <islam.guliyev@proton.me>
2025-10-18 02:24:48 +0400

    Fix

---
+++ 0x00/character-builder/Builder.java
@@ -0,0 +1,9 @@
+public interface Builder {
+    void setName(String name);
+    void setType(CharacterType type);
+    void setEndurance(int endurance);
+    void setIntelligence(int intelligence);
+    void setStrength(int strength);
+    void setDexterity(int dexterity);
+    void setResilience(int resilience);
+}
---
+++ 0x00/character-builder/MageBuilder.java
@@ -0,0 +1,77 @@
+public class MageBuilder implements Builder {
+    private String name;
+    private CharacterType type;
+    private int intelligence;
+    private int strength;
+    private int endurance;
+    private int resilience;
+    private int dexterity;
+
+
+    public Mage build() throws Exception {
+        return new Mage(name, intelligence, strength, endurance, resilience, dexterity);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CharacterType getType() {
+        return type;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public int getResilience() {
+        return resilience;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setType(CharacterType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+
+    @Override
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    @Override
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    @Override
+    public void setResilience(int resilience) {
+        this.resilience = resilience;
+    }
+}
